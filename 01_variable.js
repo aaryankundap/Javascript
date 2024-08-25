3eee69b6051d7f5cd66fef2f1cf797a22b7fb990
aaryankundap <165361545+aaryankundap@users.noreply.github.com>
2024-08-25 21:13:22 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 123
+
+//   anything in const cannot be changed
+// therefore accountId = 2 cannot be declared again 
+
+let accountemail = "aaryan@gmail.com"
+var accountpassword = "123"
+accountcity = "Pune"
+
+console.table([accountemail,accountpassword,accountcity])
+
+/*
+prefer not to use var because of issue in block scope and fucntion scope
+*/
---
+++ 02_datatype.js
@@ -0,0 +1,11 @@
+"use strict";  // treats all js code as newer version , by default present in most of the compilers 
+
+
+// alert(3+3)
+// we are using node js not browser
+// this will work only in browser
+
+
+
+console.log(typeof null);// it is object
+console.log(typeof undefined); // undefined
